075b79e851269bc8ac6dbe21eb5ba8530acd1e14
LamDV <macos@MacOSs-MacBook-Pro.local>
2025-11-12 16:02:57 +0700

    fix: update ui - bgr splash

--- lib/modules/splash/splash_screen.dart
+++ lib/modules/splash/splash_screen.dart
@@ -7,95 +7,96 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage(
-            AssetHelper.imgSplashBackground,
+    return SizedBox.expand(
+      child: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              AssetHelper.imgSplashBackground,
+            ),
+            fit: BoxFit.cover,
           ),
-          fit: BoxFit.fitWidth,
         ),
-      ),
-      child: Align(
-        alignment: Alignment.bottomCenter,
-        child: Container(
-          width: double.infinity,
-          height: getSize(300),
-          decoration: BoxDecoration(
-            color: ColorConstants.blur,
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(30),
-              topRight: Radius.circular(30),
+        child: Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            width: double.infinity,
+            height: getSize(300),
+            decoration: BoxDecoration(
+              color: ColorConstants.blur,
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
             ),
-          ),
-          child: Padding(
-            padding: EdgeInsets.only(
-              top: getSize(16),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                const Center(
-                  child: Text(
-                    "Your Journey In",
-                    style: TextStyle(
-                      fontSize: 32,
-                      color: ColorConstants.white,
-                      fontWeight: FontWeight.w800,
+            child: Padding(
+              padding: EdgeInsets.only(
+                top: getSize(16),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Center(
+                    child: Text(
+                      "Your Journey In",
+                      style: TextStyle(
+                        fontSize: 32,
+                        color: ColorConstants.white,
+                        fontWeight: FontWeight.w800,
+                      ),
                     ),
                   ),
-                ),
-                const Center(
-                  child: Text(
-                    "Your Hands",
-                    style: TextStyle(
-                      fontSize: 32,
-                      color: ColorConstants.white,
-                      fontWeight: FontWeight.w800,
+                  const Center(
+                    child: Text(
+                      "Your Hands",
+                      style: TextStyle(
+                        fontSize: 32,
+                        color: ColorConstants.white,
+                        fontWeight: FontWeight.w800,
+                      ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: getSize(24),
-                  ),
-                  child: const Text(
-                    "The best travel app in 2023,",
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: ColorConstants.subText,
-                      fontWeight: FontWeight.w600,
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: getSize(24),
+                    ),
+                    child: const Text(
+                      "The best travel app in 2023,",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: ColorConstants.subText,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: getSize(8)),
-                  child: const Text(
-                    "Answer for traveler to find their journey",
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: ColorConstants.subText,
-                      fontWeight: FontWeight.w600,
+                  Padding(
+                    padding: EdgeInsets.only(top: getSize(8)),
+                    child: const Text(
+                      "Answer for traveler to find their journey",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: ColorConstants.subText,
+                        fontWeight: FontWeight.w600,
+                      ),
                     ),
                   ),
-                ),
-                SizedBox(
-                  height: getSize(16),
-                ),
-                CircleAvatar(
-                  backgroundColor: ColorConstants.primaryColor,
-                  maxRadius: 24,
-                  child: SvgPicture.asset(
-                    AssetHelper.icoNextRight,
-                    height: getSize(32),
-                    width: getSize(32),
+                  SizedBox(
+                    height: getSize(16),
                   ),
-                ),
-                SizedBox(
-                  height: getSize(16),
-                ),
-              ],
+                  CircleAvatar(
+                    backgroundColor: ColorConstants.primaryColor,
+                    maxRadius: 24,
+                    child: SvgPicture.asset(
+                      AssetHelper.icoNextRight,
+                      height: getSize(32),
+                      width: getSize(32),
+                    ),
+                  ),
+                  SizedBox(
+                    height: getSize(16),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
